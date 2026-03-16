817c46495fdfd1e6fcd6c319b54e9ec28cedffd5
dira <nadiravisa9@gmail.com>
2026-03-16 14:35:25 +0700

    add hello world

--- src/helloworld/pkg202557201031/Helloworld202557201031.java
+++ src/helloworld/pkg202557201031/Helloworld202557201031.java
@@ -15,6 +15,7 @@ public class Helloworld202557201031 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hello world");
     }
     
 }
